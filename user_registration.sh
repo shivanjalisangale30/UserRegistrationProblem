5eebdc2b24c37f6891ce23687f5792ee3dfb17a7
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-11-21 10:43:30 +0530

    Valid last name

--- user_registration.sh
+++ user_registration.sh
@@ -3,12 +3,15 @@ echo "Welcome"
 
 shopt -s extglob
 
-echo "Enter your first name"
-read fname
+#echo "Enter your first name"
+#read fname
+echo "Enter your last name"
+read lname
 
 fname1="^[A-Z][a-zA-Z]{3,15}$"
+lname1="^[A-Z][a-zA-Z]{3,15}$"
 
- if [[ $fname =~ $fname1 ]]
+ if [[ $lname =~ $lname1 ]]
      then 
       echo yes;
  else
