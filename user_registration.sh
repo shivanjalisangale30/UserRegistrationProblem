0ca71c00968df0cd7fb46bab228d794350b06626
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-11-21 11:17:07 +0530

    Validation of email id

--- user_registration.sh
+++ user_registration.sh
@@ -7,13 +7,44 @@ echo "Enter your first name"
 read firstName
 echo "Enter your last name"
 read lastName
+echo "Enter your email id"
+read email
 
 firstName1="^[A-Z][a-zA-Z]{3,15}$"
 lastName1="^[A-Z][a-zA-Z]{3,15}$"
+email1="^([0-9a-zA-Z]+)([_+-.][0-9a-zA-Z])?[@][0-9a-zA-Z]+([.][a-z]{2,4})([.][a-z]{2})?$"
 
- if [[ $firstName =~ $firstName1 && $lastName =~ $lastName1 ]]
+
+function firstName()
+{
+  if [[ $firstName =~ $firstName1 ]]
      then 
-      echo yes;
- else
-       echo no;
- fi
+      echo "Enter valid first name"
+  else
+       echo "Enter invalid fisrt name"
+  fi
+}
+
+function lastName()
+{
+  if [[ $lastName =~ $lastName1 ]]
+     then 
+      echo "Enter valid last name"
+  else
+       echo "Enter invalid last name "
+  fi
+}
+
+function emailId()
+{
+   if [[ $email =~ $email1 ]]
+      then
+      echo "Enter valid email"
+   else
+      echo "Enter invalid email"
+   fi
+}
+
+firstName
+lastName
+emailId
